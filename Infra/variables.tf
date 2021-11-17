5a77a54fe4ab557c7c7ccecd59631a04b6cd4891
ochuko <ochuko3d@yahoo.com>
2021-11-18 00:52:45 +0100

    Update variables.tf

--- Infra/variables.tf
+++ Infra/variables.tf
@@ -3,3 +3,9 @@ variable "aws_region" {
   description = "The AWS region to create things in."
   default     = "eu-west-1"
 }
+
+variable "statebucket" {
+    description = "The name of the bucket. "
+    type        = string
+
+}
