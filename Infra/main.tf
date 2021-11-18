448e404f64850ca9f5cbe3043b27f9f328ea13a3
ochuko <ochuko3d@yahoo.com>
2021-11-18 09:30:47 +0100

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -1,20 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-  backend "s3" {
-  # bucket         = "${var.statebucket}"
-  #  key            = "state/terraform.tfstate"
-  #  region         = "eu-west-1"
-  # encrypt        = true
-  #  kms_key_id     = "alias/terraform-bucket-key"
-  # dynamodb_table = "terraform-state"
-    }
-}
-
 provider "aws" {
   region = var.aws_region
 }
